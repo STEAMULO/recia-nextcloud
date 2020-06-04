2a2768971f1453566668028299f7f5b35527ab8d
Noé <nmaireamiot@steamulo.com>
2020-06-04 18:03:39 +0200

    Intégration de la configuration du SSO CAS dans le ldap importer

---
+++ ldapimporter/ldapimporter/lib/Exception/PhpCas/PhpUserCasLibraryNotFoundException.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace OCA\LdapImporter\Exception\PhpCas;
+
+
+use OCA\LdapImporter\Exception\UserCasException;
+
+/**
+ * Class PhpUserCasLibraryNotFoundException
+ *
+ * @package OCA\LdapImporter\Exception\PhpCas
+ *
+ * @since 1.5.0
+ */
+class PhpUserCasLibraryNotFoundException extends UserCasException
+{
+}
---
+++ ldapimporter/ldapimporter/lib/Exception/UserCasException.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace OCA\LdapImporter\Exception;
+
+
+/**
+ * Class UserCasException
+ *
+ * @package OCA\LdapImporter\Exception
+ *
+ * @since 1.5.0
+ */
+class UserCasException extends \Exception
+{
+}
